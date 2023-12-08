7161f3ffe2b0bcefe6e032379fd9f1a033d3f7d4
AZKazakov21 <AZKazakov21@codingburgas.bg>
2023-12-08 21:35:28 +0200

    Create manager

---
+++ Project/Project/Manager.cpp
@@ -0,0 +1,48 @@
+#include "Manager.h"
+
+std::shared_ptr<Manager> Manager::instance = nullptr;
+
+Manager::~Manager()
+{
+    CloseWindow();
+}
+
+void Manager::Initialize()
+{
+    InitWindow(1000, 1000, "SCREEN_TITLE");
+
+    Image img = LoadImage("../assets/Logo.png");
+    SetWindowIcon(img);
+    /*ToggleFullscreen();*/
+    SetTargetFPS(60);
+    Start();
+}
+
+void Manager::Start()
+{
+    //SceneManager::GetInstance()->ChangeScene("Main Menu");
+    
+}
+
+void Manager::Update()
+{
+    //BeginDrawing();
+    //GetFPS();
+    //LoadNewScene();
+    //SceneManager::GetInstance()->UpdateScene();
+
+    //if (UIManager::GetInstance()->GetShouldFpsShow())
+    //    DrawFPS(2, 0);
+
+    //EndDrawing();
+}
+
+bool Manager::ShouldWindowClose()
+{
+    return WindowShouldClose() || shouldWindowClose;
+}
+
+void Manager::Close()
+{
+    shouldWindowClose = true;
+}
--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
-#include "raylib.h"
-int main() {
+#include "Manager.h"
 
+
+int main()
+{
+
+	std::shared_ptr<Manager> manager = Manager::GetInstance();
+
+	manager->Initialize();
+
+	while (!manager->ShouldWindowClose())
+	{
+		manager->Update();
+	}
+
+	return 0;
 }
-
-
