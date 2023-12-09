32bcc6b8752701296c2237c44ad4b6216c4599b3
AZKazakov21 <AZKazakov21@codingburgas.bg>
2023-12-09 03:11:07 +0200

    Finish main menu design

--- Project/Project/MainMenu.cpp
+++ Project/Project/MainMenu.cpp
@@ -4,22 +4,27 @@
 void MainMenu::LoadScene() { // Override for virtual LoadScene
 	auto UIManager = UIManager::GetInstance();
 
-	float buttonW = 50.0f + (MeasureText("ALTO", 100) - 200) / 2;
+	Image logoImg = LoadImage("../assets/AlTo logo.png");
+	ImageResize(&logoImg, 500, 500);
+	Texture2D logoTx = LoadTextureFromImage(logoImg);
+	UIManager->AddTexture({ logoTx, {0,0, 500, 500}, {SCREEN_WIDTH - 600, SCREEN_HEIGHT - 600 } });
+
+	float buttonX = 200.0f + (MeasureText("ALTO", 100) - 200) / 2;
 
 	//UIManager->AddButton({ {(SCREEN_WIDTH - 300) / 2 - 160,  200, 300, 100}, "PLAY", 50, LIGHTPINK, BLACK, []() {
 	//	SceneManager::GetInstance()->ChangeScene("Play");
 	//} });
 
-	UIManager->AddButton({ {buttonW, 225, 200, 75}, "PLAY", 30, LIGHTPINK, BLACK, []() {
-		SceneManager::GetInstance()->ChangeScene("PLAY");
+	UIManager->AddButton({ {buttonX, 275, 200, 75}, "PLAY", 30, LIGHTPINK, BLACK, []() {
+		SceneManager::GetInstance()->ChangeScene("Play");
 	} });
 
-	UIManager->AddButton({ {buttonW, 350, 200, 75}, "SETTINGS", 30, LIGHTPINK, BLACK, []() {
-	SceneManager::GetInstance()->ChangeScene("SETTINGS");
+	UIManager->AddButton({ {buttonX, 400, 200, 75}, "SETTINGS", 30, LIGHTPINK, BLACK, []() {
+	SceneManager::GetInstance()->ChangeScene("Settings");
 } });
 
-	UIManager->AddButton({ {buttonW, 475, 200, 75}, "QUIT", 30, LIGHTPINK, BLACK, []() {
-SceneManager::GetInstance()->ChangeScene("QUIT");
+	UIManager->AddButton({ {buttonX, 525, 200, 75}, "QUIT", 30, LIGHTPINK, BLACK, []() {
+		Manager::GetInstance()->Close();
 } });
 
 	//UIManager->AddButton({ {(SCREEN_WIDTH - 300) / 2 + 200, 200, 300, 100}, "SETTINGS", 50, LIGHTPINK, BLACK, []() {
@@ -30,7 +35,7 @@ SceneManager::GetInstance()->ChangeScene("QUIT");
 	//	Manager::GetInstance()->Close();
 	//} });
 
-	UIManager->AddText(new Text({ "ALTO", 100, {50, 50}, BLACK }));
+	UIManager->AddText(new Text({ "ALTO", 100, {200, 100}, BLACK }));
 
 	backgroundColor = OFFWHITE;
 
